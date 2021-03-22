6599cce8ca2b226edef0305a29c88bcd59e88b64
e <1181355852@qq.com>
2021-03-22 13:26:50 +0800

    游戏

--- 数字炸弹.py
+++ 数字炸弹.py
@@ -4,3 +4,22 @@
 # @工程名:PycharmStudy
 # @用户：Administrator
 # @IDE名字:PyCharm
+import sys
+import PySimpleGUI as sg
+
+class Boom():
+    def __init__(self):
+        self.layout=[
+            [sg.T('请输入数字：1-100')],
+            [sg.T(''*20)],
+            [sg.In()],
+            [sg.B('确认'),sg.B('取消'),sg.B('刷新')],
+        ]
+        pass
+
+    def mian(self):
+        pass
+
+
+if __name__ == '__main__':
+    boom =Boom()
