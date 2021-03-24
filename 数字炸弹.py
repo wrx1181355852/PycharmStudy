741149c6b4b78abdd8db5170d819a1c847658a96
e <1181355852@qq.com>
2021-03-24 11:20:15 +0800

    game

--- 数字炸弹.py
+++ 数字炸弹.py
@@ -22,9 +22,14 @@ class Boom:
         self.state_menu = 0
 
         self.minding, self.maxding = self.get_num()
+        while True:
+            if self.maxNum - self.minNum < 2:
+                self.minding, self.maxding = self.get_num()
+            else:
+                break
         self.minNum = self.minding  # 初次选择范围，之后固定
         self.maxNum = self.maxding
-        self.num = random.randint(self.minNum, self.maxNum + 1)  # 数值范围
+        self.num = random.randint(self.minNum + 1, self.maxNum - 1)  # 数值范围
 
         self.guess = 0
         self.gif = '爆炸.gif'  # 爆炸动图地址，可以替换
@@ -37,19 +42,20 @@ class Boom:
                   # size=(20,1),
                   font=('楷体', 20)
                   )],
-            [sg.T(f'  {self.minNum}-{self.maxNum}   ', key='-TEXT2-', font=('楷体', 20))],
-            [sg.In(size=(30, 1), focus=True, do_not_clear=False, key='-IN-')],
+            [sg.T(f' {self.minNum}-{self.maxNum}不包含{self.minNum}和{self.maxNum}.  ', key='-TEXT2-', font=('楷体', 20))],
+            [sg.In(size=(35, 1), focus=True, do_not_clear=False, key='-IN-')],
             [sg.B('确认', bind_return_key=True), sg.B('退出'), sg.B('刷新')],
         ]
         self.window = sg.Window('数字炸弹', self.layout, keep_on_top=True,
                                 element_justification='c',
                                 grab_anywhere=True,
-                                size=(250, 150))  # 设置窗口，置顶，居中显示，大小 250*150 标题为‘数字炸弹’
+                                size=(300, 150))  # 设置窗口，置顶，居中显示，大小 250*150 标题为‘数字炸弹’
         self.i = 0
         self.mian()
 
     def get_num(self):
         while True:
+
             try:
                 self.minNum = int(sg.popup_get_text(message='最小值:', keep_on_top=True, size=(17, 2)))
             except TypeError:
@@ -67,14 +73,16 @@ class Boom:
                     else:
                         break
                 break
+
+
+
+
         return self.minNum, self.maxNum
 
     def mian(self):
 
         while True:
             event, values = self.window.read()
-
-            # print(event, values)
 
             if event in ('退出', None):
                 break
@@ -90,48 +98,41 @@ class Boom:
                     self.guess = int(values['-IN-'])  # 转为整数赋值用于比较
                 except ValueError:
                     self.window['-TEXT1-'].update('请输入正确范围：')
-                    self.window['-TEXT2-'].update(f'  {self.minNum}-{self.maxNum}   ')
+                    self.window['-TEXT2-'].update(f'{self.minNum}-{self.maxNum}不包含{self.minNum}和{self.maxNum}.')
                 else:
-                    # print(type(self.guess), type(self.minNum), type(self.maxNum), type(self.num))
+
                     if self.num > self.guess > self.minNum:  # 猜小了，更新低点
                         self.minNum = self.guess
                         self.window['-TEXT1-'].update('猜错了！再来。')
-                        self.window['-TEXT2-'].update(f'{self.minNum}到{self.maxNum}')
+                        self.window['-TEXT2-'].update(f'{self.minNum}-{self.maxNum}不包含{self.minNum}和{self.maxNum}.')
                     elif self.num < self.guess < self.maxNum:  # 猜大了，更新顶点
                         self.maxNum = self.guess
                         self.window['-TEXT1-'].update('猜错了！再来。')
-                        self.window['-TEXT2-'].update(f'{self.minNum}到{self.maxNum}')
+                        self.window['-TEXT2-'].update(f'{self.minNum}-{self.maxNum}不包含{self.minNum}和{self.maxNum}.')
                     elif self.guess == self.num:  # 猜对了
                         self.window['-TEXT1-'].update('猜对啦！就是：')
                         self.window['-TEXT2-'].update(self.num)
+
                         Thread(target=play_sound, daemon=True).start()
                         Thread(target=self.gif_show(), daemon=True).start()
-                        # while True:    #显示炸弹GIF
-                        # time.sleep(0.01)
-                        # self.i+=1
-                        # state = sg.PopupAnimated(image_source=self.gif, time_between_frames=140)
-                        #
-                        # if not state or self.i==300:
-                        #     sg.PopupAnimated(image_source=None, )
-                        #     break
+                        self.flush()
+
                     else:
                         self.window['-TEXT1-'].update('请输入正确范围：')
-                        self.window['-TEXT2-'].update(f'{self.minNum}到{self.maxNum}')
+                        self.window['-TEXT2-'].update(f'{self.minNum}-{self.maxNum}不包含{self.minNum}和{self.maxNum}.')
 
             if event == '刷新':  # 刷新程序重新开始
-                # self.get_num()   #每次刷新选择范围
-                if self.state_menu == 0:
-                    self.start_set()
-                if self.state_menu == 1:
-                    self.every_set()
-                # self.minNum = self.minding  # 初次选择范围，之后固定
-                # self.maxNum = self.maxding
-                #
-                # self.num = random.randint(self.minNum, self.maxNum+1)
-                # self.window['-TEXT1-'].update('请输数字：')
-                # self.window['-TEXT2-'].update(f'  {self.minNum}-{self.maxNum}   ')
+                self.flush()
+
         self.window.close()
         sys.exit()
+
+    def flush(self):
+        self.i = 0
+        if self.state_menu == 0:
+            self.start_set()
+        if self.state_menu == 1:
+            self.every_set()
 
     def gif_show(self):
         while True:  # 显示炸弹GIF
@@ -147,15 +148,15 @@ class Boom:
         self.minNum = self.minding  # 初次选择范围，之后固定
         self.maxNum = self.maxding
 
-        self.num = random.randint(self.minNum, self.maxNum + 1)
+        self.num = random.randint(self.minNum + 1, self.maxNum - 1)
         self.window['-TEXT1-'].update('请输数字：')
-        self.window['-TEXT2-'].update(f'  {self.minNum}-{self.maxNum}   ')
+        self.window['-TEXT2-'].update(f'{self.minNum}-{self.maxNum}不包含{self.minNum}和{self.maxNum}.')
 
     def every_set(self):
         self.get_num()  # 每次刷新选择范围
-        self.num = random.randint(self.minNum, self.maxNum + 1)
+        self.num = random.randint(self.minNum + 1, self.maxNum - 1)
         self.window['-TEXT1-'].update('请输数字：')
-        self.window['-TEXT2-'].update(f'  {self.minNum}-{self.maxNum}   ')
+        self.window['-TEXT2-'].update(f'{self.minNum}-{self.maxNum}不包含{self.minNum}和{self.maxNum}.')
 
 
 if __name__ == '__main__':
