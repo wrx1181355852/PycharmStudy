9edd5c64d109d15c8bd9a9091f076d1a44f4868a
e <1181355852@qq.com>
2021-03-24 10:47:18 +0800

    game

--- 数字炸弹.py
+++ 数字炸弹.py
@@ -19,24 +19,31 @@ def play_sound():
 
 class Boom:
     def __init__(self):
+        self.state_menu = 0
+
         self.minding, self.maxding = self.get_num()
         self.minNum = self.minding  # 初次选择范围，之后固定
         self.maxNum = self.maxding
-        self.num = random.randint(self.minNum, self.maxNum)  # 数值范围
+        self.num = random.randint(self.minNum, self.maxNum + 1)  # 数值范围
 
         self.guess = 0
         self.gif = '爆炸.gif'  # 爆炸动图地址，可以替换
+        self.menumsg = [
+            ['模式', ['开局设置范围', '每局设置范围']]
+        ]
         self.layout = [  # 定义界面
+            [sg.Menu(self.menumsg, key='-MENU-')],
             [sg.T('   请输入数字:      ', key='-TEXT1-',
                   # size=(20,1),
                   font=('楷体', 20)
                   )],
             [sg.T(f'  {self.minNum}-{self.maxNum}   ', key='-TEXT2-', font=('楷体', 20))],
-            [sg.In(size=(30, 1), do_not_clear=False, key='-IN-')],
+            [sg.In(size=(30, 1), focus=True, do_not_clear=False, key='-IN-')],
             [sg.B('确认', bind_return_key=True), sg.B('退出'), sg.B('刷新')],
         ]
         self.window = sg.Window('数字炸弹', self.layout, keep_on_top=True,
                                 element_justification='c',
+                                grab_anywhere=True,
                                 size=(250, 150))  # 设置窗口，置顶，居中显示，大小 250*150 标题为‘数字炸弹’
         self.i = 0
         self.mian()
@@ -65,16 +72,22 @@ class Boom:
     def mian(self):
 
         while True:
-            self.event, self.values = self.window.read()
+            event, values = self.window.read()
 
-            # print(self.event, self.values)
+            # print(event, values)
 
-            if self.event in ('退出', None):
+            if event in ('退出', None):
                 break
 
-            if self.event == '确认':
+            if event == '开局设置范围':
+                self.state_menu = 0
+
+            if event == '每局设置范围':
+                self.state_menu = 1
+
+            if event == '确认':
                 try:
-                    self.guess = int(self.values['-IN-'])  # 转为整数赋值用于比较
+                    self.guess = int(values['-IN-'])  # 转为整数赋值用于比较
                 except ValueError:
                     self.window['-TEXT1-'].update('请输入正确范围：')
                     self.window['-TEXT2-'].update(f'  {self.minNum}-{self.maxNum}   ')
@@ -104,15 +117,19 @@ class Boom:
                     else:
                         self.window['-TEXT1-'].update('请输入正确范围：')
                         self.window['-TEXT2-'].update(f'{self.minNum}到{self.maxNum}')
-            if self.event == '刷新':  # 刷新程序重新开始
+
+            if event == '刷新':  # 刷新程序重新开始
                 # self.get_num()   #每次刷新选择范围
-
-                self.minNum = self.minding  # 初次选择范围，之后固定
-                self.maxNum = self.maxding
-
-                self.num = random.randint(self.minNum, self.maxNum)
-                self.window['-TEXT1-'].update('请输数字：')
-                self.window['-TEXT2-'].update(f'  {self.minNum}-{self.maxNum}   ')
+                if self.state_menu == 0:
+                    self.start_set()
+                if self.state_menu == 1:
+                    self.every_set()
+                # self.minNum = self.minding  # 初次选择范围，之后固定
+                # self.maxNum = self.maxding
+                #
+                # self.num = random.randint(self.minNum, self.maxNum+1)
+                # self.window['-TEXT1-'].update('请输数字：')
+                # self.window['-TEXT2-'].update(f'  {self.minNum}-{self.maxNum}   ')
         self.window.close()
         sys.exit()
 
@@ -125,6 +142,20 @@ class Boom:
             if not state or self.i == 310:
                 sg.PopupAnimated(image_source=None, )
                 break
+
+    def start_set(self):
+        self.minNum = self.minding  # 初次选择范围，之后固定
+        self.maxNum = self.maxding
+
+        self.num = random.randint(self.minNum, self.maxNum + 1)
+        self.window['-TEXT1-'].update('请输数字：')
+        self.window['-TEXT2-'].update(f'  {self.minNum}-{self.maxNum}   ')
+
+    def every_set(self):
+        self.get_num()  # 每次刷新选择范围
+        self.num = random.randint(self.minNum, self.maxNum + 1)
+        self.window['-TEXT1-'].update('请输数字：')
+        self.window['-TEXT2-'].update(f'  {self.minNum}-{self.maxNum}   ')
 
 
 if __name__ == '__main__':
